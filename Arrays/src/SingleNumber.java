b5f6e5a9cef398d51f7993b23bb6c05c95690237
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-23 17:39:53 +0530

    Initial commit

---
+++ Arrays/src/SingleNumber.java
@@ -0,0 +1,12 @@
+public class SingleNumber {
+
+    static int  findSingleNumber(int[] array){
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int array[] = new int[]{6,2,4,3,4,2,3};
+
+    }
+}
